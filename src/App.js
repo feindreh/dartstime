9137cfc56d5e779fd315ebf4ef85a5f1bb3b65b8
feindreh <119032218+feindreh@users.noreply.github.com>
2023-07-13 18:18:40 +0200

    add switch to switch between components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import "./css/style.css"
+import Loading from "./components/loading";
+import Lobby from "./components/lobby"
+import NewPlayer from "./components/newPlayer";
+import NewGame from "./components/newGame";
 
 function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+
+  const [state,setState] = useState(1)
+  // 1 = Loading
+  // 2 = Lobby
+  // 3 = new Player
+  // 4 = new Game
+
+  switch(state){
+    case 1 :
+      return(<div className = "App"> <Loading/> </div>)
+    case 2 : 
+      return(<div className = "App"> <Lobby/> </div>)
+    case 3 : 
+      return (<div className = "App"> <NewPlayer/> </div>)
+    case 4 :
+      return (<div className = "App"> <NewGame/> </div>)
+    default:
+      return <div>Upsi</div>
+  }
+  
 }
 
 export default App;
