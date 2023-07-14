93969869dc5a50f64327af37f031f24900a83f98
feindreh <119032218+feindreh@users.noreply.github.com>
2023-07-14 12:46:03 +0200

    add loadData to get players from firebase

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react"
+import { getAllPlayers } from "../firebase/firebase"
 function Lobby(props){
 
     const {change} = props
+
+    const loadData = async()=>{
+        const data = await getAllPlayers()
+        console.log(data)
+    }
+
+    useEffect(()=>{
+        loadData()
+    },[])
 
     return (<div>
               <button type = "button" onClick={()=>{change(4)}}>Spiel eintragen</button>
