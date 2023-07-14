93d86459adc70b3b168b2873bf0403c8ba23ff9f
feindreh <119032218+feindreh@users.noreply.github.com>
2023-07-14 13:16:45 +0200

    add makeNewPlayer

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,30 @@
 
 
 import app from "./app";
-import {getFirestore,getDocs,collection} from "firebase/firestore";
+import {getFirestore,getDocs,collection,doc,setDoc} from "firebase/firestore";
+import uniqid from 'uniqid';
 
 const db = getFirestore(app);
 
-
-
-export async function getAllPlayers(){
-    console.log("read")
-    const player = (name,elo,id) => {
+const player = (name,elo,id) => {
         return {
             name,elo,id
         }
     }
-    
-    
-    
+
+//write
+export async function makeNewPlayer(name){
+    console.log("write")
+    //check Name ?
+    const id = uniqid()
+    await setDoc(doc(db,"user",id),{
+        name:name,
+        elo:1500,
+    })
+}
+//read
+export async function getAllPlayers(){
+    console.log("read")
     const players = await getDocs(collection(db, "user"));
     const res = []
     players.forEach((doc) => {
