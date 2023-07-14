75b5b17bfbfdd87555d7b232b80124e73d64b357
feindreh <119032218+feindreh@users.noreply.github.com>
2023-07-14 16:09:37 +0200

    add select player names for newGame

--- src/components/newGame.js
+++ src/components/newGame.js
@@ -1,9 +1,51 @@
+import { getAllPlayers } from "../firebase/firebase"
+import { useState,useEffect } from "react"
+
 function NewGame(props){
     const {change} = props
-    return (<div>
+
+    const [players,setPlayers] = useState([])
+    const [playerOne,setPlayerOne] = useState("")
+    const [playerTwo,setPlayerTwo] = useState("")
+    
+    const loadData = async()=>{
+        const data = await getAllPlayers()
+        setPlayers(data)
+    }
+
+    useEffect(()=>{
+        loadData()
+    },[])
+
+
+
+    return (
+    <div>
         <div>Spiel eintragen</div>
         <button type = "button" onClick = {() => {change(2)}}>Zurück</button>
-        </div>)
+        
+        <select onChange = {(e)=>{setPlayerOne(e.target.value)}}name="pets" id="pet-select">
+            <option value="">--Please choose an option--</option>
+            {players.map((p) => {
+                return (<option key = {p.id}>
+                    {p.name}
+                </option>)
+            })}
+        </select>
+        
+        <select onChange = {(e)=>{setPlayerTwo(e.target.value)}}name="pets" id="pet-select">
+            <option value="">--Please choose an option--</option>
+            {players.map((p) => {
+                return (<option key = {p.id}>
+                    {p.name}
+                </option>)
+            })}
+        </select>
+        
+        <button type="button" onClick = {()=>{console.log(playerOne,playerTwo)}}>Bestätigen</button>
+
+    </div>
+    )
 
 
 }
