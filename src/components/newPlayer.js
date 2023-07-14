6c10ad81ae85f2b88d4ba9b27e660bb6d2343493
feindreh <119032218+feindreh@users.noreply.github.com>
2023-07-14 13:17:08 +0200

    add feat to add new players

--- src/components/newPlayer.js
+++ src/components/newPlayer.js
@@ -1,8 +1,21 @@
+import { makeNewPlayer } from "../firebase/firebase"
+import { useState } from "react"
 function NewPlayer(props){
 
   const {change} = props
+
+  const [name,setName] = useState("")
+
+  const createPlayer = async()=>{
+    await makeNewPlayer(name)
+    change(2)
+  }
+
+
   return (<div>
       <div>Neuer Spieler</div>
+      <input type="text"  onChange = {(e)=>{setName(e.target.value)}}/>
+      <button type="button" onClick={createPlayer}>Erstellen</button>
       <button type = "button" onClick = {() => {change(2)}}>Zurück</button>
       </div>)
 
